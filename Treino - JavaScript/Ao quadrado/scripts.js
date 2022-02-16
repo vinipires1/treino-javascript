0d28cdc87e7a59ba0405fa6cf446c3714509f2e7
vinipires1 <vinipires2013@gmail.com>
2022-02-16 16:15:51 -0300

    Inserindo mais um treino

---
+++ Treino - JavaScript/Ao quadrado/scripts.js
@@ -0,0 +1,23 @@
+function aoQuadrado() {
+    let a, b, c, d, e;
+    a = 10;
+    b = 5;
+    c = 20;
+    d = 9;
+    e = 3;
+
+    let quadrado = parseInt(a) * parseInt(a);
+    let quadrado1 = parseInt(b) * parseInt(b);
+    let quadrado2 = parseInt(c) * parseInt(c);
+    let quadrado3 = parseInt(d) * parseInt(d);
+    let quadrado4 = parseInt(e) * parseInt(e);
+
+    alert("Quadrado de 10 é = " + quadrado);
+    alert("Quadrado de 5 é = " + quadrado1);
+    alert("Quadrado de 20 é = " + quadrado2);
+    alert("Quadrado de 9 é = " + quadrado3);
+    alert("Quadrado de 3 é = " + quadrado4);
+
+}
+
+aoQuadrado();
